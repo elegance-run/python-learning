c1717b1a03ce57f35d7f66ec98c5514d07e5d2f7
zhengrun <zhengrun1101@163.com>
2016-11-04 21:18:38 +0800

    Function_call.py

---
+++ Function_call.py
@@ -0,0 +1,27 @@
+
+#通过自定义函数，求圆的面积：
+"""def area_of_circle(r):
+    pi=3.1415926
+    area=pi*r*r
+    return area
+
+a=int(input("请输入一个圆的半径：\n"))
+print(area_of_circle(a))
+"""
+
+#求一个元素为int类型的list里所有元素的平方和：
+"""def list_sum(List):
+    sum=0
+    for x in List:
+        sum=sum+x*x
+    return sum
+
+
+m=1
+list=[]
+while m<=100:
+   list.append(m)
+   m=m+1
+
+print(list_sum(list))
+"""
