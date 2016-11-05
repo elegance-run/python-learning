044485206c3366cec3c1bce49623d005a1aaa73a
zhengrun <zhengrun1101@163.com>
2016-11-05 21:06:12 +0800

    Tuple.py

---
+++ Tuple.py
@@ -0,0 +1,23 @@
+Tuple=('WestBrook','Durant','Paul','Curry');
+print(Tuple);
+print(Tuple[1]);
+print(Tuple[3]);
+#Tuple[0]='Kobe Bryant';
+
+
+T=(1,2,3,4,5,6,7,8,9);
+print(T);
+
+t=("Adam");
+print(t);
+t=("Adam",);
+print(t);
+
+
+My=('Anthony','Iverson',['Bryant','Gasol']);
+print(My);
+L=My[2];
+print(L);
+L[0]='Klay Tompson';
+L[1]='Kavin Durant';
+print(My);
