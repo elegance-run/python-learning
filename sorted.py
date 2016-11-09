32c3ba9f53a883f43ab41d9f4e0fdf3f962c82b0
zhengrun <zhengrun1101@163.com>
2016-11-09 16:47:19 +0800

    sorted.py

---
+++ sorted.py
@@ -0,0 +1,20 @@
+L=[('Bob',75),('Adam',92),('Bart',66),('Lisa',88)]
+
+def by_name(t):
+    return t[0]
+L1=sorted(L,key=by_name)
+print('按姓名排名：',L1)
+
+def by_score(t):
+    return t[1]
+L2=sorted(L,key=by_score)
+print('按姓名排名：',L2)
+
+
+
+
+
+
+
+
+
